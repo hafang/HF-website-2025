bacee60a40b2688bd8c0468c2913606688c2ef07
Haolin Fang <h.fang@buck.co>
2025-09-15 01:51:45 -0700

    Added Flat link

--- projectData.js
+++ projectData.js
@@ -1216,7 +1216,7 @@ const PROJECT_DATA = {
         subtitle: 'Website, JavaScript',
         description: `
         <p>
-        FLAT is a platform for theoretical and critical texts, interviews, reviews, projects, and experiments that engage with contemporary conversations surrounding emerging media in the arts. FLAT is open to submissions of writing, images, video, interactive works, and almost any possible form of web-based content from writers, artists, and thinkers around the world. Please see open calls for specific information about what themes and media we are accepting at this time. Contributors to FLAT include theorists, technologists, and artists from the UCLA community, the greater Los Angeles area, and beyond. FLAT was founded in 2018 and is designed, curated, and produced by faculty, graduate and undergraduate students, and alumnx from UCLA’s department of Design Media Arts.
+        <a href="https://flatjournal.com/" target="_blank">FLAT</a> is a platform for theoretical and critical texts, interviews, reviews, projects, and experiments that engage with contemporary conversations surrounding emerging media in the arts. FLAT is open to submissions of writing, images, video, interactive works, and almost any possible form of web-based content from writers, artists, and thinkers around the world. Please see open calls for specific information about what themes and media we are accepting at this time. Contributors to FLAT include theorists, technologists, and artists from the UCLA community, the greater Los Angeles area, and beyond. FLAT was founded in 2018 and is designed, curated, and produced by faculty, graduate and undergraduate students, and alumnx from UCLA’s department of Design Media Arts.
         </p>
 
         <p>
